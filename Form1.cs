00736569b541720a2ee23e18f93fedb260a12303
Melike <Melike@MELIKEPC>
2024-07-18 01:09:43 +0300

    Add project files.

---
+++ Form1.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace classOrnegi
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            ogrenci ogr1 = new ogrenci();
+            string adi = textBox1.Text;
+
+            ogr1.ad = adi;
+
+            ogr1.adYaz();
+        }
+    }
+}
---
+++ ogrenci.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Windows.Forms;
+namespace classOrnegi
+{
+    class ogrenci
+    {
+
+        public string ad;
+
+        public void adYaz()
+        {
+            MessageBox.Show(ad);
+        }
+    }
+}
